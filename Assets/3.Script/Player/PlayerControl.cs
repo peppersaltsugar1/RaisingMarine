9efa051fecb93d71d1e519bb85da124b6a4564a6
peppersoltsugar <128586055+peppersoltsugar@users.noreply.github.com>
2023-06-25 23:48:58 +0900

    네비메시추가

---
+++ Assets/3.Script/Player/PlayerControl.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("네비메시")]
+    private NavMeshAgent agent;  // 추가
+
+    [Header("애니메이터")]
+    private Animator animator;
+
+    [Header("플레이어 무브관련")]
+    private Camera camera;
+    private bool isMove;
+    private Vector3 destination;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                SetDestination(hit.point);
+            }
+        }
+
+        LookMoveDirection();
+    }
+
+    private void SetDestination(Vector3 dest)
+    {
+        agent.SetDestination(dest);
+        destination = dest;
+        isMove = true;
+        animator.SetBool("isMove", isMove);
+    }
+
+    // private void Move()
+    private void LookMoveDirection()  // 변경
+    {
+        if (isMove)
+        {
+            // if (Vector3.Distance(destination, transform.position) <= 0.1f)
+            if (agent.velocity.magnitude == 0.0f)  // 변경
+            {
+                isMove = false;
+                animator.SetBool("isMove", isMove);
+                return;
+            }
+
+            // var dir = destination - transform.position;
+            var dir = new Vector3(agent.steeringTarget.x, transform.position.y, agent.steeringTarget.z) - transform.position;  // 변경
+            animator.transform.forward = dir;
+        }
+    }
+}
+
