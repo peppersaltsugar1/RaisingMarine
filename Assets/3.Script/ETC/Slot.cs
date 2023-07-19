834fed4ba04fb0c958f86e7b417259affdf44ae1
peppersoltsugar <128586055+peppersoltsugar@users.noreply.github.com>
2023-07-19 22:47:59 +0900

    툴팁살짝맞춤

--- Assets/3.Script/ETC/Slot.cs
+++ Assets/3.Script/ETC/Slot.cs
@@ -7,14 +7,16 @@ public class Slot : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
     [TextArea]
     public string text;
+    [SerializeField]private PlayerControl player;
 
+    [SerializeField]private ToolTip tooltip;
     public void OnPointerEnter(PointerEventData eventData)
     {
-
+        tooltip.ShowToolTip(this);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-
+        tooltip.HideToolTip();
     }
 }
